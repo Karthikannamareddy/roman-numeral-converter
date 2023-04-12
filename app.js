39a87945e7c1a15dfc3609f5553c418ad064cc82
Karthikannamareddy <126660956+Karthikannamareddy@users.noreply.github.com>
2023-04-12 10:42:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,67 @@
 function convertToRoman(num) {
+  const romanSymbols = {
+    1000: 'M',
+    900: 'CM',
+    500: 'D',
+    400: 'CD',
+    100: 'C',
+    90: 'XC',
+    50: 'L',
+    40: 'XL',
+    10: 'X',
+    9: 'IX',
+    5: 'V',
+    4: 'IV',
+    1: 'I'
+  }
+  
+  let temp = num; 
+  let romanNum = []; 
 
-const numStr=num.toString();
-const len=numStr.length;
-let Thousands=0,Hundreds= 0,Tens= 0;
-if(len>=2){Tens=parseInt(numStr[len-1-1]);}
-if(len>=3){Hundreds=parseInt(numStr[len-1-2]);}
-if(len==4){Thousands=parseInt(numStr[0]);}
-const Ones=num%10;
-const lookup=[
-    ["","I","II","III","IV","V","VI","VII","VIII","IX"],
-    ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"],
-    ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],
-    ["","M","MM","MMM"]];
-return lookup[3][Thousands]+lookup[2][Hundreds]+
-lookup[1][Tens]+lookup[0][Ones];
-}
+  function breakNum(value) {
+    temp -= Math.floor(temp/value) * value;
+    return temp;
+  }
+  
+  
+  function addRomanNum(value) {
+      romanNum.push(romanSymbols[value].repeat((temp/value) * 1)); 
+  }
+ 
+ 
+  function romanNumerals(value) {
+    let flag; 
+    
+    
+    if (value >= 1000) {flag = 1000;}
+    else if (value >= 100) {
+      if (value >= 900) {flag = 900;}
+      else if (value >= 500) {flag = 500;}
+      else if (value >= 400) {flag = 400;}
+      else {flag = 100;}
+      }
+    else if (value >= 10) {
+      if (value >= 90) {flag = 90;}
+      else if (value >= 50) {flag = 50;}
+      else if (value >= 40) {flag = 40;}
+      else {flag = 10;}
+    }
+    else {
+      if (value >= 9) {flag = 9;}
+      else if (value >= 5) {flag = 5;}
+      else if (value >= 4) {flag = 4;}
+      else {flag = 1;}
+    }
 
+    addRomanNum(flag); 
+    temp = breakNum(flag); 
+    if (temp >= 1) { 
+       romanNumerals(temp);
+    }
+  }
+  
+  romanNumerals(num); 
+  return romanNum.join("") 
+ }
+ 
 convertToRoman(36);
